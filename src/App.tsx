f31d804e1c1ef58d507f28cab921078ec67d0a7b
cykim8811 <cykim8811@snu.ac.kr>
2024-09-23 21:54:57 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Page from "./components/Page";
+
+function App() {
+  return (
+    <div className="text-blue-500">
+      <Page />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./ui/button";
+
+export const Page: React.FC = () => {
+  return (
+    <div className="text-green-500">
+      Hello, World!
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Page;
